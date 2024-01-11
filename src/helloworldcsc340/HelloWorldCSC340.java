78cd0e20284d216dd43fc0f297c6d4e11afc2edb
Arturo D. <delafuentearturo2@gmail.com>
2024-01-11 11:22:55 -0500

    Commited after Adding

---
+++ src/helloworldcsc340/HelloWorldCSC340.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package helloworldcsc340;
+
+/**
+ *
+ * @author delaf
+ */
+public class HelloWorldCSC340 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World CSC 340");
+        
+    }
+    
+}
